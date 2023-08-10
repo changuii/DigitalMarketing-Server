eb9ac1c09cb19bd13f635da03fa7d99825c3fe80
2-jjong <jhyeon2450@gmail.com>
2023-08-10 10:41:20 +0900

    :white_check_mark: Review, Product 수정

--- server/storeServer/sales_post/src/main/java/com/example/sales_post/DAO/ProductDao.java
+++ server/storeServer/sales_post/src/main/java/com/example/sales_post/DAO/ProductDao.java
@@ -1,13 +1,14 @@
 package com.example.sales_post.DAO;
 
+import com.example.sales_post.Entity.ProductEntity;
 import org.json.simple.JSONObject;
 
 import java.util.List;
 
 public interface ProductDao {
-    JSONObject create(JSONObject jsonObject);
-    JSONObject readOne(JSONObject jsonObject);
-    List<JSONObject> readAll(JSONObject jsonObject);
-    JSONObject update(JSONObject jsonObject);
-    JSONObject delete(JSONObject jsonObject);
+    boolean create(ProductEntity productEntity);
+    ProductEntity readOne(Long productSerialNumber);
+    List<ProductEntity> readAll(Long productSerialNumber);
+    boolean update(ProductEntity productEntity);
+    boolean delete(Long productSerialNumber);
 }
