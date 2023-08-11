9fe3d29466223355db5a79fa798bd0b1c8373e94
mimijae <qzqzcarazOO@gmail.com>
2023-08-12 02:35:17 +0900

    apps.py 수정 및 주석 추가

--- server/boardServer/board/consumers.py
+++ server/boardServer/board/consumers.py
@@ -1,39 +1,44 @@
-from kafka import KafkaConsumer    
-import json
-import logging
+from kafka import KafkaConsumer  # KafkaConsumer 모듈을 가져옵니다.
+import json  # json 모듈을 가져옵니다.
+import logging  # 로깅 모듈을 가져옵니다.
 from board.views import handle_message  # views.py 내의 handle_message 함수를 import 합니다.
 
+# 로깅 설정: 정보 레벨(logging.INFO)로 로그를 출력하고, 출력 형식도 설정합니다.
 logging.basicConfig(level=logging.INFO, format="%(asctime)s %(levelname)s %(message)s")
 
-class PostKafkaConsumer:
-    def __init__(self):
+class PostKafkaConsumer:  # PostKafkaConsumer 클래스 정의 시작
+    def __init__(self):  # 생성자 함수
+        # Kafka 관련 로그를 처리하기 위한 로거 설정
         kafka_logger = logging.getLogger('kafka')
         kafka_logger.setLevel(logging.WARNING)
 
+        # 카프카 소비자 인스턴스를 생성합니다.
         self.consumer = KafkaConsumer(
-            'test2',
-            bootstrap_servers=['192.168.0.3:9092'],
-            group_id='foo',
-            auto_offset_reset='latest',
-            value_deserializer=lambda v: json.loads(v.decode('utf-8')),
-            api_version=(3, 5, 1),
+            'test2',  # topic 이름
+            bootstrap_servers=['192.168.0.3:9092'],  # Kafka broker 주소
+            group_id='foo',  # 소비자 그룹 ID
+            auto_offset_reset='latest',  # 가장 최신의 메시지부터 시작
+            value_deserializer=lambda v: json.loads(v.decode('utf-8')),  # 메시지를 JSON 형식으로 디코딩
+            api_version=(3, 5, 1),  # 카프카 API 버전
         )
 
-    def run(self):
+    def run(self):  # run 함수 정의 시작
         try:
+            # 카프카에서 메시지를 가져옵니다. 타임아웃은 1000ms(1초)입니다.
             self.consumer.poll(timeout_ms=1000)  
             logging.info("카프카 서버에 성공적으로 연결되었습니다.")
-        except Exception as ex:
+        except Exception as ex:  # 예외가 발생하면
             logging.error(f"카프카에 연결하는 중 예외 발생: {ex}")
 
-        while True:
+        # 계속해서 카프카로부터 메시지를 수신
+        while True:  # 무한 루프 시작
             try:
-                for message in self.consumer:
-                    data = message.value
+                for message in self.consumer:  # 카프카에서 메시지를 가져옵니다.
+                    data = message.value  # 메시지의 값을 data 변수에 할당합니다.
                     print(f"수신한 메시지: {data}")
                     handle_message(data)  # 메시지를 views.py의 handle_message 함수에 넘겨줍니다.
-            except KeyboardInterrupt:  
+            except KeyboardInterrupt:  # 키보드 인터럽트 (예: Ctrl+C)로 종료하려 할 때
                 logging.warning("중단됨")  
-                break  
-            except Exception as ex:  
+                break  # 무한 루프를 빠져나옵니다.
+            except Exception as ex:  # 그 외의 예외 발생 시
                 logging.error(f"메시지를 소비하는 도중 예외 발생: {ex}")
--- server/boardServer/board/views.py
+++ server/boardServer/board/views.py
@@ -1,62 +1,90 @@
-import json
-import redis
-from .serializers import PostSerializer
-from .models import Post
-import logging
+import json  # json 모듈을 임포트합니다.
+import redis  # redis 모듈을 임포트합니다.
+from .serializers import PostSerializer  # 현재 패키지의 serializers에서 PostSerializer를 임포트합니다.
+from .models import Post  # 현재 패키지의 models에서 Post 모델을 임포트합니다.
+import logging  # 로깅 모듈을 임포트합니다.
 
+# 현재 모듈의 로깅 객체를 가져옵니다.
 logger = logging.getLogger(__name__)
 
-# Redis 연결 초기화
+# Redis 서버와 연결합니다.
 redis_conn = redis.StrictRedis(host='localhost', port=6379, db=0)
 
-def save_to_redis(key, value):
+def save_to_redis(key, value):  # save_to_redis 함수를 정의합니다.
+    # 주어진 key와 value를 Redis에 저장합니다.
     redis_conn.set(key, json.dumps(value))
+    # 로그에 정보를 출력합니다.
     logger.info(f"Saved data to Redis with key {key}")
 
+# handle_message 함수를 정의합니다.
 def handle_message(data):
-    action = data.get("action", "").upper()  # 액션 값을 대문자로 가져옴
+    # data에서 "action" 키의 값을 대문자로 변환하여 가져옵니다. 기본값은 빈 문자열입니다.
+    action = data.get("action", "").upper()
+    # data에서 "requestId" 키의 값을 가져옵니다.
     request_id = data.get("requestId")
 
+    # action이 'PMPOSTCREATE'일 경우의 로직
     if action == 'PMPOSTCREATE':
+        # 데이터를 PostSerializer에 전달합니다.
         serializer = PostSerializer(data=data)
+        # 데이터가 유효한지 확인합니다.
         if serializer.is_valid():
+            # 유효하면 저장합니다.
             serializer.save()
+            # Redis에 성공 메시지를 저장합니다.
             save_to_redis(request_id, {"status": "success"})
         else:
+            # 유효하지 않으면 Redis에 실패 메시지와 에러 메시지를 저장합니다.
             save_to_redis(request_id, {"status": "failure", "message": serializer.errors})
 
+    # action이 'PMPOSTUPDATE'일 경우의 로직
     elif action == 'PMPOSTUPDATE':
         try:
+            # data에서 'pmPostNumber' 키의 값을 사용하여 Post 객체를 가져옵니다.
             post = Post.objects.get(id=data['pmPostNumber'])
+            # Post 객체와 데이터를 PostSerializer에 전달합니다.
             serializer = PostSerializer(post, data=data)
+            # 데이터가 유효한지 확인합니다.
             if serializer.is_valid():
+                # 유효하면 저장합니다.
                 serializer.save()
+                # Redis에 성공 메시지를 저장합니다.
                 save_to_redis(request_id, {"status": "success"})
         except Post.DoesNotExist:
+            # Post 객체가 존재하지 않는 경우, Redis에 실패 메시지를 저장합니다.
             save_to_redis(request_id, {"status": "failure", "message": "Post Not Found"})
 
+    # action이 'PMPOSTDELETE'일 경우의 로직
     elif action == 'PMPOSTDELETE':
         try:
+            # data에서 'pmPostNumber' 키의 값을 사용하여 Post 객체를 가져옵니다.
             post = Post.objects.get(id=data['pmPostNumber'])
+            # Post 객체를 삭제합니다.
             post.delete()
+            # Redis에 성공 메시지를 저장합니다.
             save_to_redis(request_id, {"status": "success"})
         except Post.DoesNotExist:
+            # Post 객체가 존재하지 않는 경우, Redis에 실패 메시지를 저장합니다.
             save_to_redis(request_id, {"status": "failure", "message": "Post Not Found"})
 
+    # action이 'PMPOSTREADALL'일 경우의 로직
     elif action == 'PMPOSTREADALL':
+        # 모든 Post 객체를 가져옵니다.
         posts = Post.objects.all()
+        # 모든 Post 객체를 PostSerializer에 전달합니다.
         serializer = PostSerializer(posts, many=True)
+        # 직렬화된 데이터를 가져옵니다.
         posts_data = serializer.data
-    
-        if posts_data:  # 게시물 데이터가 있을 때
+
+        # 직렬화된 데이터가 있으면 Redis에 성공 메시지와 함께 저장합니다.
+        if posts_data:
             save_to_redis(request_id, {
                 "status": "success",
                 "data": posts_data
             })
-        else:  # 게시물 데이터가 없을 때 (예: DB에 게시물이 없는 경우)
+        else:
+            # 데이터가 없으면 Redis에 실패 메시지를 저장합니다.
             save_to_redis(request_id, {
                 "status": "failure",
                 "message": "No posts found"
             })
-
-
