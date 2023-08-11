0beac096f86ab5014aab026bfe7123e1293884a3
mimijae <qzqzcarazOO@gmail.com>
2023-08-12 01:15:45 +0900

    ✅ 카프카를 통해 받은 데이터를 CRUD 처리하고(sqlite3) redis에 저장 구현 완료

--- server/boardServer/board/models.py
+++ server/boardServer/board/models.py
@@ -1,7 +1,13 @@
 from django.db import models
-import uuid
 
 class Post(models.Model):
-    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
-    title = models.CharField(max_length=200)
-    content = models.TextField(max_length=200)
+    pmCategory = models.CharField(max_length=200, blank=True)
+    pmPostTitle = models.CharField(max_length=200, blank=True)
+    pmPostWriter = models.CharField(max_length=200, blank=True)
+    pmPostDate = models.DateTimeField(auto_now_add=True)  # 날짜는 자동으로 현재 날짜와 시간을 할당합니다.
+    pmPostContents = models.TextField(blank=True)
+    pmPostPicture = models.ImageField(upload_to='posts/images/', blank=True)  # 이미지는 posts/images/ 디렉토리에 저장됩니다.
+    pmPostHitCount = models.IntegerField(default=0)  # 조회수는 기본적으로 0으로 설정합니다.
+    pmPostLike = models.IntegerField(default=0)  # 좋아요 수는 기본적으로 0으로 설정합니다.
+    pmComment = models.TextField(blank=True)  # 댓글 내용을 저장합니다.
+    storeLocation = models.CharField(max_length=300, blank=True)
